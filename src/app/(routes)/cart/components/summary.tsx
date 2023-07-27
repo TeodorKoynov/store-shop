2c60e66e1ec5ca9c79daed1884cceedc490cad1d
Teodor Koynov <teodor.koynov2005@gmail.com>
2023-07-27 13:02:00 +0300

    button disabled

--- src/app/(routes)/cart/components/summary.tsx
+++ src/app/(routes)/cart/components/summary.tsx
@@ -47,7 +47,7 @@ export const Summary: React.FC<SummaryProps> = () => {
           <Currency value={totalPrice} />
         </div>
       </div>
-      <Button onClick={onCheckout} className="mt-6 w-full">
+      <Button disabled={items.length === 0} onClick={onCheckout} className="mt-6 w-full">
         Checkout
       </Button>
     </div>
